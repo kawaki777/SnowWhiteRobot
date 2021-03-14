4cc9905c3095316478b5c13956f8c5054a566694
majid-nex <77442341+majid-nex@users.noreply.github.com>
2021-03-14 18:20:25 +0530

    Delete abuse_string.py

--- SaitamaRobot/modules/abuse_string.py
+++
@@ -1,9 +0,0 @@
-ABUSE = (
-    "fati hui gaand mat khol apni"
-    "Gaand mara bhosdike",
-    "Madarchod",
-    "Bhosdike kabhi apni shakal dekhi bhi hai",
-    "If you had to choose between going naked or having your thoughts appear in thought bubbles above your head for everyone to read, which would you choose?",
-    "teri mai 2 rupee mai kharidoga",
-    "teri maa ki chot bhosdike",  
-)
