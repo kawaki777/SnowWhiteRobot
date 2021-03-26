4c5c57d216068706b616cd419ff7372450e94974
majid-nex <77442341+majid-nex@users.noreply.github.com>
2021-03-26 14:52:44 +0530

    Update igris.py

--- SaitamaRobot/igris.py
+++ SaitamaRobot/igris.py
@@ -2,8 +2,7 @@ from telethon import events
 from SaitamaRobot import telethn
 
 """Triggers start command in pm and in groupchats"""
-def igris
-bot(**args):
+def igrisbot(**args):
     """New message."""
     pattern = args.get('pattern', None)
     r_pattern = r'^[/!]'
