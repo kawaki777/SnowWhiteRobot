08e12068381379d54849b3e3aaa1cba7a87a53ba
majid-nex <77442341+majid-nex@users.noreply.github.com>
2021-03-26 20:31:44 +0530

    Update urluploader.py

--- SaitamaRobot/modules/urluploader.py
+++ SaitamaRobot/modules/urluploader.py
@@ -28,7 +28,7 @@ async def download_coroutine(session, url, file_name, event, start, bot):
 **URL:** {}
 **File Name:** {}
 **File Size:** {}
-**© @hikigaya_robot**""".format(
+**© @Misszero_bot**""".format(
                 url,
                 os.path.basename(file_name).replace("%20", " "),
                 humanbytes(total_length),
