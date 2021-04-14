4d4fcd49d0ed6c9fbc699a769f4380c602a2f4c0
HuntingBots <69734199+HuntingBots@users.noreply.github.com>
2021-04-14 07:39:52 +0530

    Update music.py

--- SaitamaRobot/modules/music.py
+++ SaitamaRobot/modules/music.py
@@ -12,9 +12,9 @@ from tswift import Song
 from youtube_dl import YoutubeDL
 from youtubesearchpython import SearchVideos
 
-from YoneRobot.conf import get_str_key
-from YoneRobot.pyrogramee.pluginshelper import get_text, progress
-from YoneRobot import pbot
+from SaitamaRobot.conf import get_str_key
+from SaitamaRobot.pyrogramee.pluginshelper import get_text, progress
+from SaitamaRobot import pbot
 
 GENIUS = get_str_key("GENIUS_API_TOKEN", None)
 
