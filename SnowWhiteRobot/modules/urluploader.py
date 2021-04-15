d7d79705a6e04cf4eff9fcfb32a9484889b7aba2
HuntingBots <69734199+HuntingBots@users.noreply.github.com>
2021-04-15 09:34:24 +0530

    Update urluploader.py

--- SnowWhiteRobot/modules/urluploader.py
+++ SnowWhiteRobot/modules/urluploader.py
@@ -3,7 +3,7 @@ import time
 
 import aiohttp
 
-from SaitamaRobot.utils.uputils import humanbytes, time_formatter
+from SnowWhiteRobot.utils.uputils import humanbytes, time_formatter
 
 
 async def download_file(url, file_name, message, start_time, bot):
