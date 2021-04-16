c7b76d0243246581db7015a957513f13dba86326
Shamil Habeeb <72779328+shamilhabeebnelli@users.noreply.github.com>
2021-04-16 11:52:04 +0530

    Update quote.py

--- SnowWhiteRobot/modules/quote.py
+++ SnowWhiteRobot/modules/quote.py
@@ -12,7 +12,7 @@ import random
 import json
 import os
 import re
-from SbowWhiteRobot.igris import igrisbot
+from SnowWhiteRobot.igris import igrisbot
 
 COLORS = [
     "#F07975", "#F49F69", "#F9C84A", "#8CC56E", "#6CC7DC", "#80C1FA", "#BCB3F9", "#E181AC"]
