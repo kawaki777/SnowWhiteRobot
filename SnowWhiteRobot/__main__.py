b762836c40b59f67aea82d0ca6e8e013e1cee089
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-05-06 10:29:30 +0530

    Update __main__.py

--- SnowWhiteRobot/__main__.py
+++ SnowWhiteRobot/__main__.py
@@ -77,7 +77,7 @@ And the following:
     dispatcher.bot.first_name, ""
     if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n")
 
-SAITAMA_IMG = "https://telegra.ph/file/c54190bb3556f4a47d238.jpg"
+SAITAMA_IMG = "https://telegra.ph/file/701d8f34a35df3fb95524.jpg"
 
 DONATE_STRING = """Heya, glad to hear you want to donate!
 Saitama is hosted on one of Kaizoku's Servers and doesn't require any donations as of now but \
