b5606a57ef5b3187dd7a0afe3cb9de4c61c4a590
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-05-21 10:43:44 +0530

    Update misc.py

--- SnowWhiteRobot/modules/helper_funcs/misc.py
+++ SnowWhiteRobot/modules/helper_funcs/misc.py
@@ -1,6 +1,6 @@
 from typing import Dict, List
 
-from AsunaRobot import NO_LOAD
+from SnowWhiteRobot import NO_LOAD
 from telegram import MAX_MESSAGE_LENGTH, Bot, InlineKeyboardButton, ParseMode
 from telegram.error import TelegramError
 
